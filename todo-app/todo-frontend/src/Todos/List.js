c76fae3ad93014daee49c53c18ed9e979c087371
utkuzun <utkuzun50@gmail.com>
2022-11-08 21:46:05 +0300

    todo comp extracted

--- todo-app/todo-frontend/src/Todos/List.js
+++ todo-app/todo-frontend/src/Todos/List.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import SingleTodo from './SingleTodo'
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
@@ -11,37 +12,16 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
 
   return (
     <>
-      {todos.map(todo => {
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-            </span>
-          </>
-        )
-
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
-
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-            <span>
-              {todo.text} 
-            </span>
-            {todo.done ? doneInfo : notDoneInfo}
-          </div>
-        )
-      }).reduce((acc, cur) => [...acc, <hr />, cur], [])}
+      {todos
+        .map((todo, index) => (
+          <SingleTodo
+            key={index}
+            todo={todo}
+            onClickComplete={onClickComplete}
+            onClickDelete={onClickDelete}
+          />
+        ))
+        .reduce((acc, cur) => [...acc, <hr />, cur], [])}
     </>
   )
 }
