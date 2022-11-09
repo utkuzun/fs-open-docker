1ca7e24dd7bef31a5624e8e9e3020ace28093fde
utkuzun <utkuzun50@gmail.com>
2022-11-09 12:26:30 +0300

    development containers added

--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
+import './App.css'
 import TodoView from './Todos/TodoView'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <TodoView />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
